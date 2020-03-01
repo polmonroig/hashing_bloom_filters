bfca5157c2a793ffffcbe4be0ce1b37cca5f0f93
Pol Monroig <polmonroig@yahoo.es>
2020-03-01 15:59:19 +0100

    Add multiword_hash comments

--- hash/multiword_hash.h
+++ hash/multiword_hash.h
@@ -11,19 +11,41 @@
 
 #include "hash_functions/hash_function.h"
 
+/**
+ * @class Represents a hash function that recursively hashes
+ *        a sequence of integers with a specified single hash function
+ * */
 class MultiWordHash {
 
 
 public:
 
+    /**
+     * @brief Basic constructor of the class
+     * @param hash_function is the single hash function used
+     *        to compute the multi word hashing
+     * */
     explicit MultiWordHash(HashFunction const& hash_function);
 
+    /**
+     * @brief Hash function operator for a sequence of characters
+     * @param words is the sequence of characters (string) to hash
+     * @returns the hashed string
+     * */
     unsigned int operator()(std::string const& words) const;
 
+    /**
+     * @brief Hash function operator for a sequence of integers
+     * @param words contains each integer
+     * @returns the hashed integer sequence
+     * */
     unsigned int operator()(std::vector<int> const& words) const;
 
 private:
 
+    /**
+     * @brief the single hash function
+     * */
     HashFunction hash;
 
 };
