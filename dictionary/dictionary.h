7337b520674ce5e379bea4f1768b007966d37fab
Pol Monroig <polmonroig@yahoo.es>
2020-03-03 14:35:47 +0100

    Add dictionary doxygen

--- dictionary/dictionary.h
+++ dictionary/dictionary.h
@@ -1,17 +1,27 @@
-//
-// Created by pol on 3/3/20.
-//
-
 #ifndef A_DICTIONARY_H
 #define A_DICTIONARY_H
 
 
+/**
+ * @class The Dictionary class is an interface for all the hash tables
+ *        and data structures that implement a key search
+ * */
 class Dictionary {
 
 public:
 
+    /**
+     * @brief Function that inserts a value into the data structure
+     * @param value is the key to insert
+     * */
     virtual void insert(int value);
 
+
+    /**
+     * @brief Function that finds if an item is in the dictionary
+     * @param value to search
+     * @returns if found
+     * */
     virtual bool find(int value) const;
 
 };
