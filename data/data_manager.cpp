f7151f767cd7ba57cf58da25263b9a92a4d02a7a
Pol Monroig <polmonroig@yahoo.es>
2020-03-03 17:21:53 +0100

    Add file existence check before creating data
    WARNING: C++17 required

--- data/data_manager.cpp
+++ data/data_manager.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <filesystem>
+
 #include "data_manager.h"
 
 /** =================================
@@ -17,24 +19,29 @@ void DataManager::setSize(unsigned int size) {
 
 
 void DataManager::generateIntegerData(std::string const& fileName, float appearance){
-    std::ofstream file;
-    file.open(fileName + "_dict", std::ios::out);
 
-    // write dictionary
-    auto data = RandGenerator::generatePermutations(randomSeed, sequenceSize);
-    writeData(file, data);
-    file.close();
+    if(!std::filesystem::exists(fileName + "_dict")){
+        std::ofstream file;
+        std::cout << "Creating file" << std::endl;
+        // write dictionary
+        file.open(fileName + "_dict", std::ios::out);
+        auto data = RandGenerator::generatePermutations(randomSeed, sequenceSize);
+        writeData(file, data);
+        file.close();
 
-    // write text file
-    file.open(fileName + "_text" , std::ios::out);
-    data = RandGenerator::generatePermutations(randomSeed, int(float(sequenceSize) * appearance));
-    writeData(file, data);
-    randomSeed = RandGenerator::generate(randomSeed);
-    data = RandGenerator::generatePermutations(randomSeed, sequenceSize * 2);
-    writeData(file, data);
+        // write text file
+        file.open(fileName + "_text" , std::ios::out);
+        data = RandGenerator::generatePermutations(randomSeed, int(float(sequenceSize) * appearance));
+        writeData(file, data);
+        randomSeed = RandGenerator::generate(randomSeed);
+        data = RandGenerator::generatePermutations(randomSeed, sequenceSize * 2);
+        writeData(file, data);
 
-    file.close();
-
+        file.close();
+    }
+    else{
+        std::cerr << "File already exists!" << std::endl;
+    }
 
 
 }
--- experimenter.cpp
+++ experimenter.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+#include "data/data_manager.h"
 #include "experiment/experiment.h"
 #include "dictionary/bloom_filters/bloom_filters.h"
 #include "hash_functions/division_hash.h"
@@ -11,6 +13,18 @@ int main(){
     int tableSize = 100;
     int n = 500;
     int nHashFunctions = 5;
+    int seed = 4156156;
+    unsigned int size = 500;
+    float keyPercentage = 0.5;
+
+    // RANDOM DATA GENERATION
+
+    DataManager data{};
+    data.setSeed(seed);
+    data.setSize(size);
+    std::string path = "/home/pol/Documents/fib/A/data/integer/";
+    std::string file_name = path + "integer_" + std::to_string(seed);
+    data.generateIntegerData(file_name, keyPercentage);
 
     // DICTIONARY DEFINITION
 
