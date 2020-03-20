e79ed7b9256e2c320787aec0772357adeea40760
polmonroig <polmonroig@yahoo.es>
2020-03-20 13:48:52 +0100

    Fix create experiments

--- src/create_experiments.sh
+++ src/create_experiments.sh
@@ -5,20 +5,21 @@
 N=$1
 LOAD=$2
 N_HASH_F=$3
-KEY_P=$4
-INPUT=$5
-OUTPUT=$6
-N_ROUNDS=$7
+SEED=$4
+KEY_P=$5
+INPUT=$6
+OUTPUT=$7
+N_ROUNDS=$8
+
 
 
 make # create executable
 echo "Number of rounds: $N_ROUNDS"
 
-
+RANDOM=$SEED
 for (( i = 0; i < $N_ROUNDS ; i++));
 do
-    SEED=`date '+%s%N'`
-    SEED=$(($SEED %10000000))
+    SEED=($RANDOM % 10000000)
     echo "Current seed: $SEED"
     ./program $N $LOAD $N_HASH_F $SEED $KEY_P $INPUT $OUTPUT
 
